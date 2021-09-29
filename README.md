6d69ee422d7099000702a98308a202518960fd50
Keith Johnson <keithjohnson@berkeley.edu>
2021-09-28 20:35:50 -0700

    added generate.py

--- README.md
+++ README.md
@@ -1,2 +1,62 @@
 # phonwork
 Phonetics workbook
+
+This repository contains templates for generating the Phonetics Workbook as
+a static html site.
+
+## Getting started
+
+To get started with this repository, clone it:
+
+```bash
+git clone https://github.com/rsprouse/phonwork
+```
+
+The site is generated with a Python script with minimal dependencies. The
+only required package that is not in the standard library is
+[jinja2](https://jinja.palletsprojects.com/en/3.0.x/). Install it before
+trying to generate the site.
+
+## Editing and building the site
+
+Templates for the site pages are in the `_templates` directory. Templates
+with filenames beginning with an alphabetic character are rendered as content
+pages that use the overall layout determined by `_layout.html`. Other
+templates beginning with underscore may be used to include sublayouts and
+are not render as separate html files.
+
+To generate the site, do:
+
+```bash
+cd scripts
+python generate.py
+```
+
+The content pages will be render to html files in the `_site` directory.
+Before the pages are generated the existing `_site` directory will be
+cleared of existing content.
+
+## Viewing the site
+
+Use your web browser to open the files in the `_site` directory to view
+the current website.
+
+## Publishing the site
+
+If the results of viewing the site are satisfactory, push the changes to
+github and then copy the current site files to the public server. A sample
+set of commands:
+
+```bash
+# View repo status
+get status
+
+# Inspect, commit, and push changes
+git diff _templates/mypage.html
+git add templates/mypage.html
+git commit -m 'Added a section to mypage.html'
+git push
+
+# Copy site files to public server
+scp -R _site/* username@server.com:/sites/phonwork
+```
--- scripts/generate.py
+++ scripts/generate.py
@@ -3,23 +3,69 @@
 # Generate site html files.
 
 import os
+import glob
 import jinja2
 import shutil
 
-mediapath = 'assets'
-mediapath = 'https://aws.com/s3/phonwork/assets'
+templatedir = '../_templates'
+sitedir = '../_site'
+jsdir = '../_js'
+cssdir = '../_css'
 
-page1 = jinja2.Environment(
-    loader=jinja2.FileSystemLoader('../_templates')
-).get_template('page1.html').render(mediapath=mediapath)
+# Values to be interpolated into Jinja2 templates.
+cfg = {
+    'mediapath': 'https://d3uxfe7dw0hhy7.cloudfront.net/phonwork/assets'
+}
 
-index = jinja2.Environment(
-    loader=jinja2.FileSystemLoader('../_templates')
-).get_template('layout.html').render(body=page1)
+def prep_sitedir(sitedir, jsdir, cssdir):
+    '''Remove existing sitedir and recreate it.'''
+    try:
+        shutil.rmtree(sitedir)
+    except FileNotFoundError:
+        pass
+    os.mkdir(sitedir)
+    shutil.copytree(jsdir, os.path.join(sitedir, 'js'))
+    shutil.copytree(cssdir, os.path.join(sitedir, 'css'))
 
-os.mkdir('../_site')
+def make_page(bodyfile, layout, cfg, templatedir, sitedir):
+    '''
+    Generate an html page using layout.
 
-with open('../_site/index.html', 'w') as f: f.write(index)
+    Parameters
+    ----------
 
-shutil.copytree('../_js', '../_site/js')
-shutil.copytree('../_css', '../_site/css')
+    bodyfile : str
+    The input body template file.
+
+    layout : template
+    A loaded Jinja2 template for the site's overall page layout.
+
+    cfg : dict
+    A configuration dictionary for site variables to be interpolated into
+    the page body content.
+
+    templatedir : str
+    The directory path where input templates (i.e. bodyfile) are found.
+
+    sitedir : str
+    The directory path where output .html files will be written.
+    '''
+    body = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(templatedir)
+    ).get_template(bodyfile).render(cfg=cfg)
+
+    page = layout.render(body=body)
+    with open(os.path.join(sitedir, bodyfile), 'w') as f: f.write(page)
+
+if __name__ == '__main__':
+    prep_sitedir(sitedir, jsdir, cssdir)
+
+    layout = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(templatedir)
+    ).get_template('_layout.html')
+
+    for bodypath in glob.glob(os.path.join(templatedir, '*.html')):
+        bodyfile = os.path.basename(bodypath)
+        if bodyfile.startswith('_'):
+            continue
+        make_page(bodyfile, layout, cfg, templatedir, sitedir)
