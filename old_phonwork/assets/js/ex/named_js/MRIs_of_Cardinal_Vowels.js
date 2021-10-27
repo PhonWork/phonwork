12132fdb7d3b154389cdf5d1c30379535c081111
PranavJayachand <pranavjayachand@berkeley.edu>
2021-10-27 16:17:50 -0700

    Added JS script that replaces number with name and puts into a subdirectory in old_phonwork. Fixed CSV error as well.

---
+++ old_phonwork/assets/js/ex/named_js/MRIs_of_Cardinal_Vowels.js
@@ -0,0 +1,41 @@
+// must assume the existence of some variables (defined in exercises.js)
+
+exerciseName = "MRIs of Cardinal Vowels";
+mincorrect=12;
+minrun=3;
+var namebase="MG_mri_movies/";
+
+imgfiles[0] = namebase+ "back_close_rounded.png"; vidfiles[0] = namebase+"back_close_rounded"; 
+imgfiles[1] = namebase+"back_close_unrounded.png"; vidfiles[1] = namebase+"back_close_unrounded";
+imgfiles[2] = namebase+"back_closemid_rounded.png"; vidfiles[2] = namebase+"back_closemid_rounded";
+imgfiles[3] = namebase+"back_open_rounded.png"; vidfiles[3] = namebase+"back_open_rounded";
+imgfiles[4] = namebase+"back_open_unrounded.png"; vidfiles[4] = namebase+"back_open_unrounded";
+imgfiles[5] = namebase+"central_mid.png"; vidfiles[5] = namebase+"central_mid";
+imgfiles[6] = namebase+"front_close_rounded.png"; vidfiles[6] = namebase+"front_close_rounded";
+imgfiles[7] = namebase+"front_close_unrounded.png"; vidfiles[7] = namebase+"front_close_unrounded";
+imgfiles[8] = namebase+"front_closemid_unrounded.png"; vidfiles[8] = namebase+"front_closemid_unrounded";
+imgfiles[9] = namebase+"front_nearopen_unrounded.png"; vidfiles[9] = namebase+"front_nearopen_unrounded";
+imgfiles[10] = namebase+"front_openmid_rounded.png"; vidfiles[10] = namebase+"front_openmid_rounded";
+imgfiles[11] = namebase+"front_openmid_unrounded.png"; vidfiles[11] = namebase+"front_openmid_unrounded";
+
+answer[0] = 8;
+answer[1] = 16;
+answer[2] = 7;
+answer[3] = 13;
+answer[4] = 5;
+answer[5] = 19;
+answer[6] = 9;
+answer[7] = 1;
+answer[8] = 2;
+answer[9] = 20;
+answer[10] = 11;
+answer[11] = 3;
+
+
+
+
+
+
+
+
+
---
+++ old_phonwork/assets/js/ex/named_js/Saying_the_Secondary_CVs.js
@@ -0,0 +1,21 @@
+// must assume the existence of some variables (defined in exercises.js)
+
+exerciseName = "Say the Secondary Cardinal Vowels";
+mincorrect=13;
+minrun=3;
+var namebase="sounds/cardinal/";
+var n = 10;
+
+for (var i=0; i<n; i++) {
+    token = i+9;
+
+    soundfiles2[i]=namebase+"c"+token.toString();    answer[i]="yes|y";  quest[i]=token.toString();
+    soundfiles2[i+n]=namebase+"dj"+token.toString();    answer[i+n]="yes|y"; quest[i+n] = token.toString();
+}
+
+
+
+
+
+
+
