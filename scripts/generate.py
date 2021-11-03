391f97c60f6ca258b109aa1acc5985a45cbf1f4f
PranavJayachand <pranavjayachand@berkeley.edu>
2021-11-03 12:27:52 -0700

    Moved media files, fixed generate from merge

--- scripts/generate.py
+++ scripts/generate.py
@@ -7,10 +7,12 @@ import glob
 import jinja2
 import shutil
 
-templatedir = '../_templates'
-sitedir = '../_site'
-jsdir = '../_js'
-cssdir = '../_css'
+templatedir = '../templates'
+sitedir = '../site'
+jsdir = '../assets/js'
+cssdir = '../assets/css'
+enc = 'utf-8'
+images = '../images'
 
 # Values to be interpolated into Jinja2 templates.
 cfg = {
@@ -26,6 +28,7 @@ def prep_sitedir(sitedir, jsdir, cssdir):
     os.mkdir(sitedir)
     shutil.copytree(jsdir, os.path.join(sitedir, 'js'))
     shutil.copytree(cssdir, os.path.join(sitedir, 'css'))
+    shutil.copytree(images, os.path.join(sitedir, 'images'))
 
 def make_page(bodyfile, layout, cfg, templatedir, sitedir):
     '''
@@ -45,23 +48,30 @@ def make_page(bodyfile, layout, cfg, templatedir, sitedir):
     The directory path where output .html files will be written.
     '''
     #Interpolates cfg variables into bodyfile variable slots
-    body = jinja2.Environment(
+    with open(templatedir + "/" + bodyfile, encoding=enc) as f:
+        title = f.readline().strip()
+        rand = f.readline().strip()
+        main = f.read()
+        print(title, rand, main)
+    script = '"' + jsdir[2:] + "/ex/" + bodyfile[:-4] + 'js"'
+    print(script)
+    '''body = jinja2.Environment(
         loader=jinja2.FileSystemLoader(templatedir)
-    ).get_template(bodyfile).render(cfg=cfg)
+    ).get_template(bodyfile).render(cfg=cfg)'''
 
 
-    #Variables in layout.render are the variables set out by the _layout html file
+    #Variables in layout.render are the variables set out by the _base_page html file
     #Differs from just body in that it also includes menu and other webpage items?
-    page = layout.render(body=body)         
+    page = layout.render(title = title, rand = rand, main = main, custom_script = script)         
     
-    with open(os.path.join(sitedir, bodyfile), 'w') as f: f.write(page)
+    with open(os.path.join(sitedir, bodyfile), 'w', encoding=enc) as f: f.write(page)
 
 if __name__ == '__main__':
     prep_sitedir(sitedir, jsdir, cssdir)
 
     layout = jinja2.Environment(
         loader=jinja2.FileSystemLoader(templatedir)
-    ).get_template('_layout.html')
+    ).get_template('_base_page.html')
 
     for bodypath in glob.glob(os.path.join(templatedir, '*.html')):
         bodyfile = os.path.basename(bodypath)
